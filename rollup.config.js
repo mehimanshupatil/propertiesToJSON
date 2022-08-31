2030869e8ac3ba1cfd935beb1532131bbf23801a
Himanshu Patil <himanshupatil744@gmail.com>
2022-08-31 14:02:03 +0530

    changes to handle some edge cases

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
     input: 'src/index.ts',
-    output:[ {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      exports: 'default'
-    },
-    {
-        file: 'dist/index.mjs',
-        format: 'es'
-      }],
-      plugins: [typescript()]
-  };
+    output: [
+        {
+            file: 'dist/index.cjs',
+            format: 'cjs',
+            exports: 'default',
+        },
+        {
+            file: 'dist/index.mjs',
+            format: 'es',
+        },
+    ],
+    plugins: [nodeResolve(), typescript()],
+};
