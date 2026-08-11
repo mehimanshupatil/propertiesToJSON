393aa1f015d081ec3b7529887fd9b3b928242357
Himanshu Patil <dev@himanshupatil.dev>
2026-08-11 10:37:14 +0530

    updated packagejson

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitest/config';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     build: {
@@ -9,7 +8,6 @@ export default defineConfig({
             fileName: (format) => (format === 'es' ? 'index.mjs' : 'index.cjs'),
         },
     },
-    plugins: [dts({ outDir: 'dist' })],
     test: {
         include: ['test/**/*.test.ts'],
     },
